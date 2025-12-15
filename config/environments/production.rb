725a941d00f3949a0a8779133c061093af6fd3e9
Gabriel Quesada <gabriel@venture360.co>
2025-12-15 22:09:29 +0100

    Allow Railway host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << "heroku-log-drain-production.up.railway.app"
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
@@ -77,5 +78,4 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts << "heroku-log-drain-production.up.railway.app"
 end
