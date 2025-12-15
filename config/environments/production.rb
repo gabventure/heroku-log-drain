b87a07650f9dffab31935fa0235f59f1eae70a66
Gabriel Quesada <gabriel@venture360.co>
2025-12-15 22:18:16 +0100

    Allow Railway host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "heroku-log-drain-production.up.railway.app"
-
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
@@ -24,6 +21,8 @@ Rails.application.configure do
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
+
+  config.hosts << /.*\.railway\.app/
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
